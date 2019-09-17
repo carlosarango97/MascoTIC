a9e8cc09d25f016f76c006568f0cb4068daa2430
carlosarango97 <47753930+carlosarango97@users.noreply.github.com>
2019-09-17 13:48:44 -0500

    Schedule interface
    In this commit I complete schedules interface and I implemented the navigation between "Main menu" and "Schedules"

--- js/main.js
+++ js/main.js
@@ -3,29 +3,29 @@
 //      pageOut: The section ID that you go out
 //      pageIn: The section ID that you go in
 function page(pageOut, pageIn){
-    document.getElementById(pageOut).className += "invisible";
+    document.getElementById(pageOut).className += " invisible";
     document.getElementById(pageIn).classList.remove("invisible");
     window.scroll(0,document.getElementById(pageIn).scrollTop);
 }
 
 /* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+function openNav(section) {
+    document.getElementById("mySidenav_" + section).style.width = "250px";
 }
 
 /* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+function closeNav(section) {
+    document.getElementById("mySidenav_" + section).style.width = "0";
 } 
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+function openNav(section) {
+    document.getElementById("mySidenav_" + section).style.width = "250px";
+    document.getElementById("main_" + section).style.marginLeft = "250px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+function closeNav(section) {
+    document.getElementById("mySidenav_" + section).style.width = "0";
+    document.getElementById("main_" + section).style.marginLeft = "0";
 } 
