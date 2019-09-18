b302cc7b95ebd528701b5c74e761646bd6ec3d60
carlosarango97 <47753930+carlosarango97@users.noreply.github.com>
2019-09-17 20:33:48 -0500

    I stared the inteface develop
    I stared to work in the "Stadistics" inteface.

--- js/main.js
+++ js/main.js
@@ -29,3 +29,42 @@ function closeNav(section) {
     document.getElementById("mySidenav_" + section).style.width = "0";
     document.getElementById("main_" + section).style.marginLeft = "0";
 } 
+
+// var Chart = require('chart.js');
+var ctx = document.getElementById('stadistics_graph').getContext('2d');
+var stadistics = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['12/9', '13/9', '14/9', '15/9', '16/9', '17/9'],
+        datasets: [{
+            label: 'Feeding',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
